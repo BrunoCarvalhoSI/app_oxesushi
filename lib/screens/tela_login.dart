693714785c344b647d361c7c943affe5561bc6d5
Bruno Carvalho <brunocarvalhoifs@gmail.com>
2023-02-16 13:18:59 -0300

    tela de login quase pronta

--- lib/screens/tela_login.dart
+++ lib/screens/tela_login.dart
@@ -1,6 +1,8 @@
 // ignore: implementation_imports
+import 'dart:js_util';
+
 import 'package:flutter/material.dart';
-import 'package:oxesushi_v1/src/componentes/custom_text_field.dart';
+import '../src/componentes/custom_text_field.dart';
 
 class Login extends StatefulWidget {
   const Login({Key key}) : super(key: key);
@@ -16,6 +18,7 @@ class _LoginState extends State<Login> {
       backgroundColor: Colors.black,
       body: Column(
         children: [
+          //CONTAINER SUPERIOR
           Expanded(
             child: Container(
               decoration: const BoxDecoration(
@@ -26,44 +29,112 @@ class _LoginState extends State<Login> {
               ),
             ),
           ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 32,
-                vertical: 40,
+          //FORMULARIO
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 32,
+              vertical: 40,
+            ),
+            //DECORACAO DO CONTAINER INFERIOR
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(45),
               ),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(45),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                //EMAIL
+                const CustomTextField(
+                  icon: Icons.email,
+                  label: "Email",
                 ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  const CustomTextField(
-                    icon: Icons.email,
-                    label: "Email",
+                //SENHA
+                const CustomTextField(
+                  icon: Icons.lock,
+                  label: "Senha",
+                  isSecret: true,
+                ),
+                //BOTAO ENTRAR
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "Entrar",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
                   ),
-                  const CustomTextField(
-                    icon: Icons.lock,
-                    label: "Senha",
-                    isSecret: true,
+                ),
+                //BOTAO ESQUECI A SENHA
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Esqueci a Senha",
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
                   ),
-                  SizedBox(
-                    height: 50,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
+                ),
+                //DIVISOR OU
+                Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 10,
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Colors.black.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 15,
+                        ),
+                        child: Text("Ou"),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: Colors.black.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                //BOTAO NOVA CONTA
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton(
+                      style: OutlinedButton.styleFrom(
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(18),
                         ),
+                        side: const BorderSide(
+                          width: 2,
+                          color: Colors.black,
+                        ),
                       ),
                       onPressed: () {},
-                      child: Text("Entrar"),
-                    ),
-                  )
-                ],
-              ),
+                      child: const Text(
+                        "Criar Conta",
+                        style: TextStyle(fontSize: 18),
+                      )),
+                )
+              ],
             ),
           ),
         ],
